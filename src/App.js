7c19d898c2ee622778ef7f969aa2e67f10fdf8f9
masayuki hiraoka <masayuki.hiraoka@ietty.co.jp>
2019-06-21 18:28:43 +0900

    add auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
@@ -23,4 +28,49 @@ function App() {
   );
 }
 
-export default App;
+const signUpConfig = {
+  header: 'My Customized Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'UserName',
+      key: 'username',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: false,
+      displayOrder: 2,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 3,
+      type: 'password'
+    },
+    {
+      label: 'PhoneNumber',
+      key: 'phone_number',
+      required: false,
+      displayOrder: 4,
+      type: 'string'
+    },
+    {
+      label: 'Custom Attribute',
+      key: 'custom_attr',
+      required: false,
+      displayOrder: 5,
+      type: 'string',
+      custom: true
+    }
+  ]
+};
+
+// export default App;
+export default withAuthenticator(App, { signUpConfig });
